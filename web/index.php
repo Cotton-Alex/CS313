701bd2cb6e1460415cfa2b02ef97d2ed5ddbc391
Alex <cot15011@byui.edu>
2018-09-22 21:10:25 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,16 @@
 <?php
 
-  phpinfo();
+  p
+    hpinfo();
 
 ?>
+<html>
+    <head>
+        <title>cs313</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>Hello World</div>
+    </body>
+</html>
