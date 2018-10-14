4eb0a7f6c603ed9574fa33875af570c86021ad3a
Alex <cot15011@byui.edu>
2018-10-14 00:34:08 -0700

    header top margin correction

--- web/mod/head.php
+++ web/mod/head.php
@@ -6,8 +6,8 @@
 <script src="https://cotton-app.herokuapp.com/js.js"></script>
 
 
-<!--
-<meta charset = "utf-8">
+
+<!--<meta charset = "utf-8">
 <meta name = "viewport" content = "width=device-width, initial-scale=1">
 <link href = "//fonts.googleapis.com/css?family=Raleway:400,300,600" rel = "stylesheet" type = "text/css">
 <link href="http://localhost/cs313-php/web/css/normalize.css" rel="stylesheet" type="text/css"/>
--- web/mod/header.php
+++ web/mod/header.php
@@ -1,6 +1,5 @@
 <nav>
     <ul>
-        <li class="offset-by-ten"></li>
         <li><a class="one columns" href='https://cotton-app.herokuapp.com/index.php'>Home</a></li>
         <li><a class="one columns" href='https://cotton-app.herokuapp.com/wk01.php'>wk01</a></li>
         <li><a class="one columns" href='https://cotton-app.herokuapp.com/wk02.php'>wk02</a></li>
@@ -10,12 +9,10 @@
 </nav>
 <!--<nav>
     <ul>
-        <li class="offset-by-ten"></li>
         <li><a class="one columns" href='http://localhost/cs313-php/web/index.php'>Home</a></li>
         <li><a class="one columns" href='http://localhost/cs313-php/web/wk01.php'>wk01</a></li>
         <li><a class="one columns" href='http://localhost/cs313-php/web/wk02.php'>wk02</a></li>
         <li><a class="one columns" href='http://localhost/cs313-php/web/w03/g08wk03_form.php'>wk03</a></li>
 		<li><a class="one columns" href='http://localhost/cs313-php/web/w04/products.php'>wk04</a></li>
-
     </ul>
 </nav>-->
