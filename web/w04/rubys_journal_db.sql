47d1b8ecbdde467efc97ebcccc017dea67688032
Alex <cot15011@byui.edu>
2018-10-15 01:51:46 -0700

    rubys_journal_db.sql - update rubys_journal_db created on heroku - again

--- web/w04/rubys_journal_db.sql
+++ web/w04/rubys_journal_db.sql
@@ -1,20 +1,21 @@
+DROP TABLE entry, journal, image;
 
-CREATE TABLE entries (
-  entry_id int primary key,
-  journal_id int references journals(journal_id),
-  page_date date,
-  image_id int references images(image_id),
-  entry_date date,
-  entry_data varchar(999)
-);
-
-CREATE TABLE journals(
+CREATE TABLE journal (
 	journal_id int primary key,
 	journal_name varchar(20)
 );
 
-CREATE TABLE images(
+CREATE TABLE image (
 	image_id int primary key,
 	image_name varchar(45),
 	page_date date
+);
+
+CREATE TABLE entry (
+  entry_id int primary key,
+  journal_id int references journal(journal_id),
+  page_date date,
+  image_id int references image(image_id),
+  entry_date date,
+  entry_data varchar(999)
 );
