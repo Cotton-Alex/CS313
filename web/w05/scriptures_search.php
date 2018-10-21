709abf8773e2b1bde9ebc7ea026dffe8cc4aaa08
Alex <cot15011@byui.edu>
2018-10-21 14:09:14 -0700

    YAY! I fixed the search feature and added volume_name

--- web/w05/scriptures_search.php
+++ web/w05/scriptures_search.php
@@ -44,7 +44,7 @@
                         INNER JOIN w05_grp_volume
                         ON w05_grp_scripture.volume_name = w05_grp_volume.volume_id
                         WHERE w05_grp_book.book_name ILIKE '%" . htmlspecialchars($_POST['book']) . "%';") as $row) {
-                        echo "<a href='scripturedisplay.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " ~ " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
+                        echo "<a href='scripturedisplay.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " - " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
                         echo '</a><br>';
                     }
                 } else {
