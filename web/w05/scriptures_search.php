a6c048fe9189223f5305921476d8574c44e78af8
Alex <cot15011@byui.edu>
2018-10-21 13:23:59 -0700

    testing

--- web/w05/scriptures_search.php
+++ web/w05/scriptures_search.php
@@ -42,8 +42,8 @@
                         ON w05_grp_scripture.book_name = w05_grp_book.book_id
                         INNER JOIN w05_grp_volume
                         ON w05_grp_scripture.volume_name = w05_grp_volume.volume_id
-                        w05_grp_book.book_name ILIKE '%" . htmlspecialchars($_POST['book_name']) . "%';") as $row) {
-                        echo "<a href='scripturedisplay.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
+                        WHERE w05_grp_book.book_name ILIKE '%" . htmlspecialchars($_POST['book_name']) . "%';") as $row) {
+                        echo "<a href='scripturedisplay.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " ~ " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
                         echo '</a><br>';
                     }
                 } else {
@@ -59,7 +59,7 @@
                         ON w05_grp_scripture.book_name = w05_grp_book.book_id
                         INNER JOIN w05_grp_volume
                         ON w05_grp_scripture.volume_name = w05_grp_volume.volume_id;') as $row) {
-                        echo "<a href='scriptures.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
+                        echo "<a href='scriptures.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " - " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
                         echo '</a><br>';
                     }
                 }
