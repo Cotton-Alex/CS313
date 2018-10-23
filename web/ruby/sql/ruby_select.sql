3d02aaedb6c92d5ac1bd882071468200865e5dbe
Alex <cot15011@byui.edu>
2018-10-23 00:03:08 -0700

    added ruby site

--- web/ruby/sql/ruby_select.sql
+++ web/ruby/sql/ruby_select.sql
@@ -1,9 +1,12 @@
 SELECT
 journal.journal_name,
+image.image_name,
 entry.page_date, 
 entry.image_id, 
 entry.entry_date, 
 entry.entry_text
 FROM entry
+INNER JOIN image
+ON entry.image_id = image.image_id
 INNER JOIN journal
 ON entry.journal_id = journal.journal_id;
