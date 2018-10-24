14f3b560b97c7c555915844854baa1b1598d4413
Alex <cot15011@byui.edu>
2018-10-24 15:44:28 -0700

    query fix

--- web/ruby/1946-1950.php
+++ web/ruby/1946-1950.php
@@ -26,60 +26,6 @@
 
                     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                    //echo '<h1>Scripture Resources</h1>';
-//					for ($db->query('SELECT
-//						journal.journal_name,
-//						image.image_name,
-//						entry.image_id,
-//						FROM entry
-//						INNER JOIN image
-//						ON entry.image_id = image.image_id
-//						INNER JOIN journal
-//						ON entry.journal_id = journal.journal_id
-//                        WHERE journal.journal_name =' . "'1951-1955'" . ';') instanceof $image) {
-//						echo '<img src="http://www.rubysjournal.com/images/' . $image["image_id"] . '.jpg" alt= Ruby"' . '' . 's 1946-1950 journal" />';
-//						echo '</a><br>';
-//					}
-//                    $image = ($db->query('SELECT
-//                        journal.journal_name,
-//                        image.image_name,
-//                        entry.page_date, 
-//                        entry.image_id, 
-//                        entry.entry_date, 
-//                        entry.entry_text
-//                        FROM entry
-//                        INNER JOIN image
-//                        ON entry.image_id = image.image_id
-//                        INNER JOIN journal
-//                        ON entry.journal_id = journal.journal_id
-//                        WHERE journal.journal_name = ' . "'1946-1950'" . ' 
-//                        ORDER BY entry.entry_id DESC
-//                        LIMIT 1 ;'));
-//                    echo '<br>';
-//                    echo '<img id="journal_page" src="http://www.rubysjournal.com/images/' . $image['image_name'] . '" alt=' . '"' . 'Ruby' . '' . 's 1946-1950 journal" />';
-//                    echo '<br>';
-//					$queryString = "SELECT";
-//					$queryString .= " journal.journal_name, image.image_name, entry.page_date, entry.image_id, entry.entry_date, entry.entry_text";
-//					$queryString .= " FROM entry";
-//					$queryString .= " INNER JOIN image";
-//					$queryString .= " ON entry.image_id = image.image_id";
-//					$queryString .= " INNER JOIN journal";
-//					$queryString .= " ON entry.journal_id = journal.journal_id";
-//					$queryString .= " WHERE journal.journal_name =" . "'1946-1950'" . 'ORDER BY entry.entry_id DESC LIMIT 1;';
-//
-//					$query = $db->prepare($queryString);
-//					$query->execute();
-//					$results = $query->fetchAll();
-//                                        echo $results;
-//					echo '<br>';
-//                                        echo '<p>journal.journal_name = ' . $results['journal.journal_name'] . '</p>';
-//                                        echo '<p>image.image_name = ' . $results['image.image_name'] . '</p>';
-//                                        echo '<p>entry.page_date = ' . $results['entry.page_date'] . '</p>';
-//                                        echo '<p>entry.image_id = ' . $results['entry.image_id'] . '</p>';
-//                                        echo '<p>entry.image_name = ' . $results['entry.image_name'] . '</p>';
-//                                        echo '<p>entry.entry_text = ' . $results['entry.entry_text'] . '</p>';
-//					echo '<img id="journal_page" src="http://www.rubysjournal.com/images/' . $results['entry.image_name'] . '" alt=' . '"' . 'Ruby' . '' . 's 1946-1950 journal" />';
-//					echo '<br>';
 
                     foreach ($db->query('SELECT
                         journal.journal_name,
@@ -101,6 +47,7 @@
                         echo '<br>';
                     }
                     ?>
+
                     <div id="journal_text">
                         <section>
                             <table>
@@ -108,21 +55,21 @@
 
                                     <?php
                                     foreach ($db->query('SELECT
-                        journal.journal_name,
-                        image.image_name,
-                        entry.page_date, 
-                        entry.image_id, 
-                        entry.entry_date, 
-                        entry.entry_text
-                        FROM entry
-                        INNER JOIN image
-                        ON entry.image_id = image.image_id
-                        INNER JOIN journal
-                        ON entry.journal_id = journal.journal_id
-                        WHERE journal.journal_name =' . "'1946-1950'" . ';') as $row) {
+                                        journal.journal_name,
+                                        image.image_name,
+                                        entry.page_date, 
+                                        entry.image_id, 
+                                        entry.entry_date, 
+                                        entry.entry_text
+                                        FROM entry
+                                        INNER JOIN image
+                                        ON entry.image_id = image.image_id
+                                        INNER JOIN journal
+                                        ON entry.journal_id = journal.journal_id
+                                        WHERE journal.journal_name =' . "'1946-1950'" . ';') as $row) {
                                         echo '<tr>';
                                         echo '<td id="tdDate">' . $row['entry_date'] . '</td>';
-                                        echo '<td> "' . $row['entry_text'] . '"</td>';
+                                        echo '<td>' . $row['entry_text'] . '</td>';
                                         echo '<tr>';
                                     }
                                 } catch (PDOException $ex) {
@@ -130,6 +77,7 @@
                                     die();
                                 }
                                 ?>
+
                             </tbody>
                         </table>
                     </section>
