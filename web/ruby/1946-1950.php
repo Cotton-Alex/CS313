b434efd9a3865fe895267b5e9a6e01808c836293
Alex <cot15011@byui.edu>
2018-10-23 20:49:40 -0700

    ruby displayoøØ

--- web/ruby/1946-1950.php
+++ web/ruby/1946-1950.php
@@ -12,23 +12,22 @@
             <div class="container">
 				<?php
 				try {
-					$dbUrl = getenv('DATABASE_URL');
+				$dbUrl = getenv('DATABASE_URL');
 
-					$dbOpts = parse_url($dbUrl);
+				$dbOpts = parse_url($dbUrl);
 
-					$dbHost = $dbOpts["host"];
-					$dbPort = $dbOpts["port"];
-					$dbUser = $dbOpts["user"];
-					$dbPassword = $dbOpts["pass"];
-					$dbName = ltrim($dbOpts["path"], '/');
+				$dbHost = $dbOpts["host"];
+				$dbPort = $dbOpts["port"];
+				$dbUser = $dbOpts["user"];
+				$dbPassword = $dbOpts["pass"];
+				$dbName = ltrim($dbOpts["path"], '/');
 
-					$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-					//echo '<h1>Scripture Resources</h1>';
-
-					foreach ($db->query('SELECT
+				//echo '<h1>Scripture Resources</h1>';
+				($db->query('SELECT
 						journal.journal_name,
 						image.image_name,
 						entry.page_date, 
@@ -40,29 +39,30 @@
 						ON entry.image_id = image.image_id
 						INNER JOIN journal
 						ON entry.journal_id = journal.journal_id
-                        WHERE journal.journal_name =' . "'1946-1950'" . ';') as $row) {
-						echo "<a href='scriptures.php?id=" . $row["entry_id"] . "'><span class='bold'>" . $row['entry_date'] . " - " . $row['entry_text'] . "</span>";
-						echo '</a><br>';
-					}
+                        WHERE journal.journal_name =' . "'1951-1955'" . ';') instanceof $image);
+				echo '<img src=' . '"http://www.rubysjournal.com/images/' . $image["entry_id"] . '.jpg" alt= Ruby' . '' . 's 1946-1950 journal />';
+				echo '</a><br>';
+						
 
-					foreach ($db->query('SELECT
-                        w05_grp_scripture.scripture_id,
-                        w05_grp_volume.volume_name,
-                        w05_grp_book.book_name,
-                        w05_grp_scripture.chapter, 
-                        w05_grp_scripture.verse,
-                        w05_grp_scripture.content
-                        FROM w05_grp_scripture
-                        INNER JOIN w05_grp_book
-                        ON w05_grp_scripture.book_name = w05_grp_book.book_id
-                        INNER JOIN w05_grp_volume
-                        ON w05_grp_scripture.volume_name = w05_grp_volume.volume_id;') as $row) {
-						echo "<a href='scriptures.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " - " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
-						echo '</a><br>';
-					}
+				foreach ($db->query('SELECT
+						journal.journal_name,
+						image.image_name,
+						entry.page_date, 
+						entry.image_id, 
+						entry.entry_date, 
+						entry.entry_text
+						FROM entry
+						INNER JOIN image
+						ON entry.image_id = image.image_id
+						INNER JOIN journal
+						ON entry.journal_id = journal.journal_id
+                        WHERE journal.journal_name =' . "'1951-1955'" . ';') as $row) {
+				echo "<a href='scriptures.php?id=" . $row["entry_id"] . "'><span class='bold'>" . $row['entry_date'] . " - " . $row['entry_text'] . "</span>";
+				echo '</a><br>';
+				}
 				} catch (PDOException $ex) {
-					echo 'Error!: ' . $ex->getMessage();
-					die();
+				echo 'Error!: ' . $ex->getMessage();
+				die();
 				}
 				?>
 
@@ -73,7 +73,7 @@
                         <button type="submit">Click Me</button>
                     </form>
                 </div>
-				</div>
+			</div>
         </main>
 		<?php require '../ruby/mod/footer.php'; ?>
     </body>
