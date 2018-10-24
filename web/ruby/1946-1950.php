07bfcf1cab5a0ba3f14455f0ed6c19aaf56f2424
Alex <cot15011@byui.edu>
2018-10-23 19:40:57 -0700

    ruby display

--- web/ruby/1946-1950.php
+++ web/ruby/1946-1950.php
@@ -2,52 +2,50 @@
 <html lang="en">
     <head>
         <title>Ruby's Journal | Home</title>
-        <?php require '../ruby/mod/head.php'; ?>
+		<?php require '../ruby/mod/head.php'; ?>
     </head>
     <body>
         <header id="page_header">
-            <?php require '../ruby/mod/header.php'; ?>
+			<?php require '../ruby/mod/header.php'; ?>
         </header>
-        <main>Í
+        <main>
             <div class="container">
-                <?php
-                try {
-                    $dbUrl = getenv('DATABASE_URL');
+				<?php
+				try {
+					$dbUrl = getenv('DATABASE_URL');
 
-                    $dbOpts = parse_url($dbUrl);
+					$dbOpts = parse_url($dbUrl);
 
-                    $dbHost = $dbOpts["host"];
-                    $dbPort = $dbOpts["port"];
-                    $dbUser = $dbOpts["user"];
-                    $dbPassword = $dbOpts["pass"];
-                    $dbName = ltrim($dbOpts["path"], '/');
+					$dbHost = $dbOpts["host"];
+					$dbPort = $dbOpts["port"];
+					$dbUser = $dbOpts["user"];
+					$dbPassword = $dbOpts["pass"];
+					$dbName = ltrim($dbOpts["path"], '/');
 
-                    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+					$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-                    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                    echo '<h1>Scripture Resources</h1>';
+					//echo '<h1>Scripture Resources</h1>';
 
-                    if (isset($_POST['book'])) {
-                        foreach ($db->query("SELECT
-journal.journal_name,
-image.image_name,
-entry.page_date, 
-entry.image_id, 
-entry.entry_date, 
-entry.entry_text
-FROM entry
-INNER JOIN image
-ON entry.image_id = image.image_id
-INNER JOIN journal
-ON entry.journal_id = journal.journal_id
-                                WHERE w05_grp_book.book_name ILIKE '%" . htmlspecialchars($_POST['book']) . "%';") as $row) {
-                            echo "<a href='scriptures.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " - " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
-                            echo '</a><br>';
-                        }
-                    } else {
+					foreach ($db->query("SELECT
+						journal.journal_name,
+						image.image_name,
+						entry.page_date, 
+						entry.image_id, 
+						entry.entry_date, 
+						entry.entry_text
+						FROM entry
+						INNER JOIN image
+						ON entry.image_id = image.image_id
+						INNER JOIN journal
+						ON entry.journal_id = journal.journal_id
+                        WHERE journal.journal_name =" . htmlspecialchars($_POST['1946-1950']) . ";") as $row) {
+						echo "<a href='scriptures.php?id=" . $row["entry_id"] . "'><span class='bold'>" . $row['entry_date'] . " - " . $row['entry_text'] . "</span>";
+						echo '</a><br>';
+					}
 
-                        foreach ($db->query('SELECT
+					foreach ($db->query('SELECT
                         w05_grp_scripture.scripture_id,
                         w05_grp_volume.volume_name,
                         w05_grp_book.book_name,
@@ -59,15 +57,14 @@ ON entry.journal_id = journal.journal_id
                         ON w05_grp_scripture.book_name = w05_grp_book.book_id
                         INNER JOIN w05_grp_volume
                         ON w05_grp_scripture.volume_name = w05_grp_volume.volume_id;') as $row) {
-                            echo "<a href='scriptures.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " - " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
-                            echo '</a><br>';
-                        }
-                    }
-                } catch (PDOException $ex) {
-                    echo 'Error!: ' . $ex->getMessage();
-                    die();
-                }
-                ?>
+						echo "<a href='scriptures.php?id=" . $row["scripture_id"] . "'><span class='bold'>" . $row['volume_name'] . " - " . $row['book_name'] . " " . $row['chapter'] . ":" . $row['verse'] . "</span>";
+						echo '</a><br>';
+					}
+				} catch (PDOException $ex) {
+					echo 'Error!: ' . $ex->getMessage();
+					die();
+				}
+				?>
 
                 <div>
                     <form action="scriptures_search.php" method="post">
@@ -77,6 +74,6 @@ ON entry.journal_id = journal.journal_id
                     </form>
                 </div>
         </main>
-        <?php require '../ruby/mod/footer.php'; ?>
+		<?php require '../ruby/mod/footer.php'; ?>
     </body>
 </html>
