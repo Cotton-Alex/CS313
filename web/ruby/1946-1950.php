c0ff1a3e8f27c34407e45cd9c2d551c46cfc974c
Alex <cot15011@byui.edu>
2018-10-23 20:10:42 -0700

    ruby displayoøØ

--- web/ruby/1946-1950.php
+++ web/ruby/1946-1950.php
@@ -28,22 +28,22 @@
 
 					//echo '<h1>Scripture Resources</h1>';
 
-//					foreach ($db->query("SELECT
-//						journal.journal_name,
-//						image.image_name,
-//						entry.page_date, 
-//						entry.image_id, 
-//						entry.entry_date, 
-//						entry.entry_text
-//						FROM entry
-//						INNER JOIN image
-//						ON entry.image_id = image.image_id
-//						INNER JOIN journal
-//						ON entry.journal_id = journal.journal_id
-//                        WHERE journal.journal_id = 1946-1950);") as $row) {
-//						echo "<a href='scriptures.php?id=" . $row["entry_id"] . "'><span class='bold'>" . $row['entry_date'] . " - " . $row['entry_text'] . "</span>";
-//						echo '</a><br>';
-//					}
+					foreach ($db->query('SELECT
+						journal.journal_name,
+						image.image_name,
+						entry.page_date, 
+						entry.image_id, 
+						entry.entry_date, 
+						entry.entry_text
+						FROM entry
+						INNER JOIN image
+						ON entry.image_id = image.image_id
+						INNER JOIN journal
+						ON entry.journal_id = journal.journal_id
+                        WHERE journal.journal_id = 1946-1950);') as $row) {
+						echo "<a href='scriptures.php?id=" . $row["entry_id"] . "'><span class='bold'>" . $row['entry_date'] . " - " . $row['entry_text'] . "</span>";
+						echo '</a><br>';
+					}
 
 					foreach ($db->query('SELECT
                         w05_grp_scripture.scripture_id,
