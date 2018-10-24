563ee2f1faedd00c546cf905aa5ef43e62845822
Alex <cot15011@byui.edu>
2018-10-23 23:23:58 -0700

    ruby displayoøØ

---
+++ web/w06/teach06/InsertData.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once("DatabaseConnect.php");
+
+$book = $_POST["book"];
+$chapter = $_POST["chapter"];
+$verse = $_POST["verse"];
+$content = $_POST["content"];
+
+$topics = $_POST["topics"];
+
+
+$queryString = "select insert_scripture(";
+$queryString .= "'" . $book . "'";
+$queryString .= ", " . $chapter;
+$queryString .= ", " . $verse;
+$queryString .= ", '" . $content . "'";
+$queryString .= ")";
+
+$query = $db->prepare($queryString);
+$query->execute();
+$results = $query->fetchAll();
+
+if (isset($_POST["new_topic"]) && $_POST["topic"] != "") {
+	$queryString = "select add_topic('" . $_POST["topic"] . "')";
+	$query = $db->prepare($queryString);
+	$query->execute();
+	$new_topic = $query->fetchAll();
+	$new_topic = $new_topic[0]["add_topic"];
+	array_push($topics, $new_topic);
+}
+
+foreach ($topics as $id) {
+	$queryString = "select connect_to_topic(";
+	$queryString .= $results[0]["insert_scripture"];
+	$queryString .= ", " . $id;
+	$queryString .= ")";
+
+	$query = $db->prepare($queryString);
+	$query->execute();
+	$success = $query->fetchAll();
+}
+
+header("Location: Scriptures.php");
+exit;
+?>
