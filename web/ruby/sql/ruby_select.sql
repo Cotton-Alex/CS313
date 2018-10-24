523d9e33d5d3b07d9091bb39e1c6fd821a0ff2d9
Alex <cot15011@byui.edu>
2018-10-24 11:17:35 -0700

    query fix

--- web/ruby/sql/ruby_select.sql
+++ web/ruby/sql/ruby_select.sql
@@ -10,3 +10,18 @@ INNER JOIN image
 ON entry.image_id = image.image_id
 INNER JOIN journal
 ON entry.journal_id = journal.journal_id;
+
+SELECT
+journal.journal_name,
+image.image_name,
+entry.page_date, 
+entry.image_id, 
+entry.entry_date, 
+entry.entry_text
+FROM entry
+INNER JOIN image
+ON entry.image_id = image.image_id
+INNER JOIN journal
+ON entry.journal_id = journal.journal_id
+ORDER BY entry.entry_id DESC
+LIMIT 1;
