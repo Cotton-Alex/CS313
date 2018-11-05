b6140d11523f5f1aa4fb0f3f7f5191ecba495939
Alex <cot15011@byui.edu>
2018-11-04 23:05:48 -0800

    teach week 07

--- web/mod/header.php
+++ web/mod/header.php
@@ -7,7 +7,7 @@
         <li><a class="one columns" href='https://cotton-app.herokuapp.com/w03/g08wk03_form.php'>wk03</a></li>
         <li><a class="one columns" href='https://cotton-app.herokuapp.com/w04/products.php'>wk04</a></li>
         <li><a class="one columns" href='https://cotton-app.herokuapp.com/w05/scriptures_search.php'>wk05</a></li>
-		<li><a class="one columns" href='https://cotton-app.herokuapp.com/w06/scriptures.php'>wk06</a></li>
+		<li><a class="one columns" href='https://cotton-app.herokuapp.com/w06/Scriptures.php'>wk06</a></li>
 		<li><a class="one columns" href='https://cotton-app.herokuapp.com/w07/welcome.php'>wk07</a></li>
     </ul>
 </nav>
@@ -20,7 +20,7 @@
         <li><a class="one columns" href='http://localhost/cs313-php/web/w03/g08wk03_form.php'>wk03</a></li>
 		<li><a class="one columns" href='http://localhost/cs313-php/web/w04/products.php'>wk04</a></li>
 		<li><a class="one columns" href='http://localhost/cs313-php/web/w05/scriptures_search.php'>wk05</a></li>
-		<li><a class="one columns" href='http://localhost/cs313-php/web/w06/scriptures.php'>wk06</a></li>
+		<li><a class="one columns" href='http://localhost/cs313-php/web/w06/Scriptures.php'>wk06</a></li>
 		<li><a class="one columns" href='http://localhost/cs313-php/web/w07/welcome.php'>wk07</a></li>
     </ul>
 </nav>-->
