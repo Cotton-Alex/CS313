d8e6e83ca5414cf56054e0d278d80215e8f31074
Alex <cot15011@byui.edu>
2018-11-05 00:53:07 -0800

    add date selector

--- web/ruby/date_selector.php
+++ web/ruby/date_selector.php
@@ -1,71 +1,76 @@
-<span>
-     <select name="month">
-        <option value="01">January</option>
-        <option value="02" disabled>February</option>
-        <option value="03" disabled>March</option>
-        <option value="04" disabled>April</option>
-        <option value="05" disabled>May</option>
-        <option value="06" disabled>June</option>
-        <option value="07" disabled>July</option>
-        <option value="08" disabled>August</option>
-        <option value="09" disabled>September</option>
-        <option value="10" disabled>October</option>
-        <option value="11" disabled>November</option>
-        <option value="12" disabled>December</option>
-     </select> 
-</span>
-<span>
-     <select name="day">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-          <option value="13">13</option>
-          <option value="14">14</option>
-          <option value="15">15</option>
-          <option value="16">16</option>
-          <option value="17">17</option>
-          <option value="18" disabled>18</option>
-          <option value="19" disabled>19</option>
-          <option value="20" disabled>20</option>
-          <option value="21" disabled>21</option>
-          <option value="22" disabled>22</option>
-          <option value="23" disabled>23</option>
-          <option value="24" disabled>24</option>
-          <option value="25" disabled>25</option>
-          <option value="26" disabled>26</option>
-          <option value="27" disabled>27</option>
-          <option value="28" disabled>28</option>
-          <option value="29" disabled>29</option>
-          <option value="30" disabled>30</option>
-          <option value="31" disabled>31</option>
-     </select>
-</span>
-<span>
-     <select name="day">
-          <option value="1">1946-1950</option>
-          <option value="2" disabled>1951-1955</option>
-          <option value="3" disabled>1956-1960</option>
-          <option value="4" disabled>1961-1965</option>
-          <option value="5" disabled>1966-1968</option>
-          <option value="6" disabled>1969-1973</option>
-          <option value="7" disabled>1975-1978</option>
-          <option value="8" disabled>1979-1981</option>
-          <option value="9" disabled>1982-1984</option>
-          <option value="10" disabled>1985-1987</option>
-          <option value="11" disabled>1988</option>
-          <option value="12" disabled>1989</option>
-          <option value="13" disabled>1990</option>
-          <option value="14" disabled>1991</option>
-          <option value="15" disabled>1992-1993</option>
-          <option value="16" disabled>1994</option>
-     </select>
-</span>
+<form id="date_selector">
+	<span>
+		<select name="date_selector_month">
+			<option value="01">January</option>
+			<option value="02" disabled>February</option>
+			<option value="03" disabled>March</option>
+			<option value="04" disabled>April</option>
+			<option value="05" disabled>May</option>
+			<option value="06" disabled>June</option>
+			<option value="07" disabled>July</option>
+			<option value="08" disabled>August</option>
+			<option value="09" disabled>September</option>
+			<option value="10" disabled>October</option>
+			<option value="11" disabled>November</option>
+			<option value="12" disabled>December</option>
+		</select> 
+	</span>
+	<span>
+		<select name="date_selector_day">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+			<option value="9">9</option>
+			<option value="10">10</option>
+			<option value="11">11</option>
+			<option value="12">12</option>
+			<option value="13">13</option>
+			<option value="14">14</option>
+			<option value="15">15</option>
+			<option value="16">16</option>
+			<option value="17">17</option>
+			<option value="18" disabled>18</option>
+			<option value="19" disabled>19</option>
+			<option value="20" disabled>20</option>
+			<option value="21" disabled>21</option>
+			<option value="22" disabled>22</option>
+			<option value="23" disabled>23</option>
+			<option value="24" disabled>24</option>
+			<option value="25" disabled>25</option>
+			<option value="26" disabled>26</option>
+			<option value="27" disabled>27</option>
+			<option value="28" disabled>28</option>
+			<option value="29" disabled>29</option>
+			<option value="30" disabled>30</option>
+			<option value="31" disabled>31</option>
+		</select>
+	</span>
+	<span>
+		<select name="date_selector_journal">
+			<option value="1">1946-1950</option>
+			<option value="2" disabled>1951-1955</option>
+			<option value="3" disabled>1956-1960</option>
+			<option value="4" disabled>1961-1965</option>
+			<option value="5" disabled>1966-1968</option>
+			<option value="6" disabled>1969-1973</option>
+			<option value="7" disabled>1975-1978</option>
+			<option value="8" disabled>1979-1981</option>
+			<option value="9" disabled>1982-1984</option>
+			<option value="10" disabled>1985-1987</option>
+			<option value="11" disabled>1988</option>
+			<option value="12" disabled>1989</option>
+			<option value="13" disabled>1990</option>
+			<option value="14" disabled>1991</option>
+			<option value="15" disabled>1992-1993</option>
+			<option value="16" disabled>1994</option>
+		</select>
+	</span>
+	<span>
+		<input type="submit" name="submit" value="Go">
+	</span>
+</form>
