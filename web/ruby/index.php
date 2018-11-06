5e443ef5ce9b97fbb4e859e70cd11ade3bc2bb54
Alex <cot15011@byui.edu>
2018-11-06 00:19:33 -0800

    scripture naming issue

--- web/ruby/index.php
+++ web/ruby/index.php
@@ -11,7 +11,7 @@
         <main>
             <div id="journal_covers">
                 <!--Working Journal Covers-->
-                <figure> <a href="1946-1950.php" name="1956-1950"><img src="http://www.rubysjournal.com/images/1946-1950-001-258x300.jpg" alt="Ruby's 1946-1950 journal" /> </a>
+                <figure> <a href="read/?action=1946-1950" name="1956-1950"><img src="http://www.rubysjournal.com/images/1946-1950-001-258x300.jpg" alt="Ruby's 1946-1950 journal" /> </a>
                     <figcaption>1946-1950</figcaption>
                 </figure>
                 <figure> <!--<a href="read/?action=1951-1955">--> <img src="http://www.rubysjournal.com/images/1951-1955-001-246x300.jpg" alt="Ruby's 1951-1955 Journal" />
