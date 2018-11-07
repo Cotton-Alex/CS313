036b6055217f88362d45679a2ac6713e4270485c
Alex <cot15011@byui.edu>
2018-11-07 10:18:03 -0800

    last minute stuff

--- web/ruby/mod/header.php
+++ web/ruby/mod/header.php
@@ -1,12 +1,12 @@
 <div id="top_bar">
     <!--<div id="top_bar_container">-->
         <!--<div id="top_bar_float_fix_container">-->
-            <div id="logo_container"><a href="../index.php">Ruby's Journal</a></div>
+            <div id="logo_container"><a href="https://cotton-app.herokuapp.com/ruby/index.php">Ruby's Journal</a></div>
             <div id="nav_container">
                 <nav>
                     <ul>
-                        <!--<li><a href="./index.php">JournalsOld</a></li>-->
-                        <li>
+                        <li><a href="https://cotton-app.herokuapp.com/ruby/index.php">Journals</a></li>
+<!--                        <li>
                             <div class="dropdown">
                                 <a class="dropbtn" href="../index.php">Journals</a>
                                 <div class="dropdown-content" id="myDropdown">
@@ -18,11 +18,9 @@
                                     <a href="../read/?action=1969-1973">1969-1973</a>
                                 </div>
                             </div>
-                        </li>
-                        <!--<li><a href="#">Family</a></li>-->
-                        <li><a href="../transcribe/?action=load_transcribe">Transcribe</a></li>
-                        <li><a href="../about.php">About</a></li>
-                        <!--<li><a href="/contact.php">Contact</a></li>-->
+                        </li>-->
+                        <li><a href="https://cotton-app.herokuapp.com/ruby/transcribe.php">Transcribe</a></li>
+                        <li><a href="https://cotton-app.herokuapp.com/rubyabout.php">About</a></li>
                     </ul>
                 </nav>
             </div>
