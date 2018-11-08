c2ed375946606d8bb3803dd3fe5cb22cfffa0e35
Alex <cot15011@byui.edu>
2018-11-07 16:49:17 -0800

    last minute stuff

--- web/ruby/insert_text.php
+++ web/ruby/insert_text.php
@@ -1,18 +1,30 @@
 <?php
-function add_entry($journal_id, $image_id, $entry_date, $entry_text) {
-    global $db;
-    $query = 'INSERT INTO entry
-                 ($journal_id, $imageID, $pageSideID, $entryDate, $entryData)
-              VALUES
-                 (:journal_id, :imageID, :pageSideID, :entryDate, :entryData)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':journal_id', $journal_id);
-    $statement->bindValue(':image_id', $image_id);
-    $statement->bindValue(':entry_date', $entry_date);
-    $statement->bindValue(':entry_text', $entry_text);
-    $statement->execute();
-    $statement->closeCursor();
-}
+
+$journal_id = htmlspecialchars($_POST['journal_id']);
+$page_date = htmlspecialchars($_POST['page_date']);
+$image_id = htmlspecialchars($_POST['image_id']);
+$entry_date = htmlspecialchars($_POST['entry_date']);
+$entry_text = htmlspecialchars($_POST['entry_text']);
+
+
+echo "$journal_id\n";
+echo $entry_text;
+
+//function add_entry($journal_id, $page_date, $image_id, $entry_date, $entry_text) {
+//    global $db;
+//    $query = 'INSERT INTO entry
+//                 ($journal_id, $page_date, $image_id, $entry_date, $entry_text)
+//              VALUES
+//                 (:journal_id, :page_date, :image_id, :entryD_date, :entry_text)';
+//    $statement = $db->prepare($query);
+//    $statement->bindValue(':journal_id', $journal_id);
+//    $statement->bindValue(':page_date', $page_date);
+//    $statement->bindValue(':image_id', $image_id);
+//    $statement->bindValue(':entry_date', $entry_date);
+//    $statement->bindValue(':entry_text', $entry_text);
+//    $statement->execute();
+//    $statement->closeCursor();
+//}
 
 
 
--- web/ruby/transcribe.php
+++ web/ruby/transcribe.php
@@ -52,18 +52,15 @@ $image_file_name = ($journal_name . '-' . $journal_month . '-' . $journal_day . 
                 <div id="journal_text"> 
                     <h3>Add Journal Entry</h3>
                     <form method="post" action="insert_text.php">
-                        <input type="hidden" name="journal_name" value="<?php echo $journal_name ?>">
-                        <input type="hidden" name="journal_month" value="<?php echo $journal_month ?>">
-                        <input type="hidden" name="journal_day" value="<?php echo $journal_day ?>">
-                        <input type="hidden" value="imageID" name="imageID" />
-                        <input type="hidden" value="2" name="pageSideID" />
+                        <input type="hidden" name="journal_id" value="<?php echo $journal_id ?>">
+                        <input type="hidden" name="page_date" value="<?php echo $page_date ?>">
+                        <input type="hidden" name="image_id" value="<?php echo $image_id ?>">
+                        <input type="hidden" name="entry_date" value="<?php echo $entry_date ?>">
                         <br>
                         <label class="labelDate">Date:</label>
-                        <input class="inputDate" type="date" name="entryDate" value="<?php echo $row['image.page_date'] ?>" />
+                        <input class="inputDate" type="date" name="entry_date" value="" />
                         <br>
-
-                        <!--<label>Entry:</label>-->
-                        <textarea class="transcribeTxtarea" name="entryData" rows="5" cols="44" wrap="soft" style="overflow:auto">add entry here</textarea>
+                        <textarea class="transcribeTxtarea" name="entry_text" rows="5" cols="44" wrap="soft" style="overflow:auto">add entry here</textarea>
                         <br>
 
                         <label>&nbsp;</label>
@@ -71,38 +68,6 @@ $image_file_name = ($journal_name . '-' . $journal_month . '-' . $journal_day . 
                         <br>
                     </form>
                 </div>
-
-                <!--                <div id="journal_text">
-                                    <section>
-                                        <table>
-                                            <tbody>
-                
-                                                                                //<?php
-//								foreach ($db->query('SELECT
-//                                        journal.journal_name,
-//                                        image.image_name,
-//                                        entry.page_date, 
-//                                        entry.image_id, 
-//                                        entry.entry_date, 
-//                                        entry.entry_text
-//                                        FROM entry
-//                                        INNER JOIN image
-//                                        ON entry.image_id = image.image_id
-//                                        INNER JOIN journal
-//                                        ON entry.journal_id = journal.journal_id
-//                                        WHERE image.image_name = ' . "'" . $journal_file_name . "'" . ';') as $row) {
-//									echo '<tr>';
-//									echo '<td id="tdDate">' . $row['entry_date'] . '</td>';
-//									echo '<td>' . $row['entry_text'] . '</td>';
-//									echo '<tr>';
-//								}
-//								
-                            ?>
-                
-                                            </tbody>
-                                        </table>
-                                    </section>
-                                </div>-->
             </div>
         </main>
         <?php require '../ruby/mod/footer.php'; ?>
