49d5d7256d90734c21527ff612b1aa5dce40dd95
Alex <cot15011@byui.edu>
2018-11-09 01:26:22 -0800

    last minute stuff

--- web/ruby/transcribe.php
+++ web/ruby/transcribe.php
@@ -70,10 +70,10 @@ $image_file_name = ($journal_name . '-' . $journal_month . '-' . $journal_day . 
                                         WHERE image.image_name = ' . "'" . $image_file_name . "'" . ';') as $row) {
 									echo '<div id="journal_text">';
 									echo '<form method = "post" action = "insert_text.php">';
-									echo '<input type = "hidden" name = "journal_id" value = "<?php echo' . $journal_id . '?>">';
-									echo '<input type = "hidden" name = "page_date" value = "<?php echo' . $page_date . '?>">';
-									echo '<input type = "hidden" name = "image_id" value = "<?php echo' . $image_id . '?>">';
-									echo '<input type = "hidden" name = "entry_date" value = "<?php echo' . $entry_date . '?>">';
+									echo '<input type = "hidden" name = "journal_id" value = "' . $row[$journal_id] . '">';
+									echo '<input type = "hidden" name = "page_date" value = "' . $row[$page_date] . '">';
+									echo '<input type = "hidden" name = "image_id" value = "' . $row[$image_id] . '">';
+									echo '<input type = "hidden" name = "entry_date" value = "' . $row[$entry_date] . '">';
 									//echo '<tr>';
 									//echo '<td id="tdDate">' . $row['entry_date'] . '</td>';
 									//echo '<tr>';
@@ -82,9 +82,9 @@ $image_file_name = ($journal_name . '-' . $journal_month . '-' . $journal_day . 
 									echo '<br>';
 									echo '<textarea class="transcribeTxtarea" name="entry_text" rows="5" cols="44" wrap="soft" style="overflow:auto">' . $row['entry_text'] . '</textarea>';
 									echo '<br>';
-									echo '<tr>';
+									//echo '<tr>';
 									echo '<input type="submit" value="Add Entry" />';
-									echo '<br>';
+									//echo '<br>';
 									echo '</form>';
 									echo '</div>';
 								}
@@ -94,25 +94,6 @@ $image_file_name = ($journal_name . '-' . $journal_month . '-' . $journal_day . 
                         </table>
                     </section>
                 </div>
-<!--                <div id="journal_text"> 
-
-                    <form method="post" action="insert_text.php">
-                        <input type="hidden" name="journal_id" value="<?//php echo $journal_id ?>">
-                        <input type="hidden" name="page_date" value="<?//php echo $page_date ?>">
-                        <input type="hidden" name="image_id" value="<?//php echo $image_id ?>">
-                        <input type="hidden" name="entry_date" value="<?//php echo $entry_date ?>">
-                        <br>
-                        <label class="labelDate">Date:</label>
-                        <input class="inputDate" type="date" name="entry_date" value='1946-01-01' />
-                        <br>
-                        <textarea class="transcribeTxtarea" name="entry_text" rows="5" cols="44" wrap="soft" style="overflow:auto"></textarea>
-                        <br>
-
-                        <label>&nbsp;</label>
-                        <input type="submit" value="Add Entry" />
-                        <br>
-                    </form>
-                </div>-->
             </div>
         </main>
 		<?php require '../ruby/mod/footer.php'; ?>
