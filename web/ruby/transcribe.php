c82235b052920dd880dbef98657c4f28e2c49d4e
Alex <cot15011@byui.edu>
2018-11-09 00:51:02 -0800

    last minute stuff

--- web/ruby/transcribe.php
+++ web/ruby/transcribe.php
@@ -97,10 +97,10 @@ $image_file_name = ($journal_name . '-' . $journal_month . '-' . $journal_day . 
 <!--                <div id="journal_text"> 
 
                     <form method="post" action="insert_text.php">
-                        <input type="hidden" name="journal_id" value="<?php echo $journal_id ?>">
-                        <input type="hidden" name="page_date" value="<?php echo $page_date ?>">
-                        <input type="hidden" name="image_id" value="<?php echo $image_id ?>">
-                        <input type="hidden" name="entry_date" value="<?php echo $entry_date ?>">
+                        <input type="hidden" name="journal_id" value="<?//php echo $journal_id ?>">
+                        <input type="hidden" name="page_date" value="<?//php echo $page_date ?>">
+                        <input type="hidden" name="image_id" value="<?//php echo $image_id ?>">
+                        <input type="hidden" name="entry_date" value="<?//php echo $entry_date ?>">
                         <br>
                         <label class="labelDate">Date:</label>
                         <input class="inputDate" type="date" name="entry_date" value='1946-01-01' />
