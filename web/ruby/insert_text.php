ae23104a747571512c252d0406cc1f6de7c4a2bc
Alex <cot15011@byui.edu>
2018-11-09 03:26:10 -0800

    last minute stuff

--- web/ruby/insert_text.php
+++ web/ruby/insert_text.php
@@ -1,12 +1,11 @@
-<?php
+<?php session_start() ?>
 
+<?php
 //if (!isset (htmlspecialchars($_POST['journal_id']))) {
 //    $journal_id = "1";
 //} else {
 //    $journal_id = htmlspecialchars($_POST['journal_id']);
 //}
-
-
 //echo substr($image_file_name, 0, 4); 
 
 $journal_id = htmlspecialchars($_POST['journal_id']);
@@ -14,7 +13,6 @@ $page_date = htmlspecialchars($_POST['page_date']);
 $image_id = htmlspecialchars($_POST['image_id']);
 $entry_date = htmlspecialchars($_POST['entry_date']);
 $entry_text = htmlspecialchars($_POST['entry_text']);
-$image_name = htmlspecialchars($_POST['image_name']);
 
 
 echo "$journal_id\n";
@@ -22,7 +20,17 @@ echo "$page_date\n";
 echo "$image_id\n";
 echo "$entry_date\n";
 echo "$entry_text\n";
-echo "$image_name\n";
+
+require('connect.php');
+$stmt = $db->prepare('UPDATE entry SET entry_text = :entry_text WHERE :entry_date = :entry_date');
+$stmt->bindValue(':entry_text', $entry_text, PDO::PARAM_STR);
+$stmt->bindValue(':entry_date', $entry_date, PDO::PARAM_STR);
+$stmt->execute();
+
+$new_page = "read.php?journal_month=" . $_SESSION['journal_month'] . "&journal_day=" . $_SESSION['journal_day'] . "&journal_name=1946-1950&submit=Go";
+
+header("Location: $new_page");
+die();
 
 
 
@@ -32,9 +40,9 @@ echo "$image_name\n";
 //$newdate = date ( 'Y-m-j' , $newdate );
 //echo $newdate;
 
-
-
-
+	
+		
+		
 //function add_entry($journal_id, $page_date, $image_id, $entry_date, $entry_text) {
 //    global $db;
 //    $query = 'INSERT INTO entry
@@ -50,7 +58,5 @@ echo "$image_name\n";
 //    $statement->execute();
 //    $statement->closeCursor();
 //}
-
-
 //include('transcribe.php');
 ?>
